56c8b3e4ef3da0aea4dc042f828a68f542f23b24
DannyBoyNYC <daniel.deverell@gmail.com>
2019-05-22 17:05:01 -0400

    clean

--- src/components/navbar/Item.js
+++ src/components/navbar/Item.js
@@ -1,8 +1,6 @@
 import React from "react";
 import styled from "styled-components";
 
-import { ResponsiveComponent } from "../responsive-component/react-responsive-component";
-
 const MenulinkA = styled.a`
   display: inline-block;
   padding: 0.25rem 0;
@@ -17,23 +15,3 @@ function SubmenuItem(props) {
 }
 
 export default SubmenuItem;
-
-// import React from 'react';
-// import styled from 'styled-components';
-
-// const MenulinkA = styled.a`
-//   display: inline-block;
-//   padding: 0.25rem 0;
-// `;
-
-// function SubmenuItem(props) {
-//   return (
-//     <li>
-//       <MenulinkA href={props.link}>
-//         {props.link}
-//       </MenulinkA>
-//     </li>
-//   );
-// }
-
-// export default SubmenuItem;
