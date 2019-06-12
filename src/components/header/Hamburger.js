3c1837c45714c8791d9d0f0ab7c0a2feaaa1824d
DannyBoyNYC <daniel.deverell@gmail.com>
2019-06-12 04:31:24 -0400

    done

--- src/components/header/Hamburger.js
+++ src/components/header/Hamburger.js
@@ -5,10 +5,10 @@ import { HamContainer, HamBox, HamInner } from './styles';
 function Hamburger(props) {
   let [vis, setVis] = useState(false);
 
-  const handleVisibility = e => {
-    e.preventDefault();
-    setState((state.vis = !state.vis));
-  };
+  // const handleVisibility = e => {
+  //   e.preventDefault();
+  //   setState((state.vis = !state.vis));
+  // };
 
   return (
     <div className="oops" onClick={() => setVis((vis = !vis))}>
